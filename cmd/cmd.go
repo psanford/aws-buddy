96b922b7370ab01f3b1649194c61be87de57b04e
Peter Sanford <psanford@sanford.io>
2020-02-26 09:08:43 -0800

    Add bash completion generation

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -19,5 +19,28 @@ func Execute() error {
 	}
 
 	rootCmd.AddCommand(ec2Command())
+	rootCmd.AddCommand(completionCommand())
+
 	return rootCmd.Execute()
+}
+
+func completionCommand() *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "completion",
+		Short: "Generates bash completion scripts",
+		Long: `To load completion run
+
+. <(aws-buddy completion)
+
+To configure your bash shell to load completions for each session add to your bashrc
+
+# ~/.bashrc or ~/.profile
+. <(aws-buddy completion)
+`,
+		Run: func(cmd *cobra.Command, args []string) {
+			rootCmd.GenBashCompletion(os.Stdout)
+		},
+	}
+
+	return cmd
 }
