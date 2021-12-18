9625206d5a6d7bc45e5dcfae4618321c32f20efc
Peter Sanford <psanford@sanford.io>
2021-12-17 17:12:53 -0800

    Add config query_by_id command

--- awsconfig/awsconfig.go
+++ awsconfig/awsconfig.go
@@ -15,15 +15,16 @@ func Command() *cobra.Command {
 		Short: "AWS Config Commands",
 	}
 
-	cmd.AddCommand(queryCommand())
+	cmd.AddCommand(queryPublicIPCommand())
+	cmd.AddCommand(queryResourceIDCommand())
 	return &cmd
 }
 
-func queryCommand() *cobra.Command {
+func queryPublicIPCommand() *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "query_eni_by_public_ip",
 		Short: "Query for ENIs matching a public ip",
-		Run:   queryAction,
+		Run:   queryPublicIPAction,
 	}
 
 	cmd.Flags().StringVarP(&aggregatorName, "aggregator-name", "", "AllAccounts", "AWS Config Aggretator Name")
@@ -35,7 +36,7 @@ var (
 	aggregatorName string
 )
 
-func queryAction(cmd *cobra.Command, args []string) {
+func queryPublicIPAction(cmd *cobra.Command, args []string) {
 	if len(args) == 0 {
 		log.Fatalf("Usage: query_eni_by_public_ip <PUBLIC_IP>")
 	}
@@ -60,6 +61,60 @@ WHERE
 `
 
 	query := fmt.Sprintf(queryTmpl, publicIP)
+
+	input := &configservice.SelectAggregateResourceConfigInput{
+		ConfigurationAggregatorName: &aggregatorName,
+		Expression:                  &query,
+	}
+	err := svc.SelectAggregateResourceConfigPages(input, func(resp *configservice.SelectAggregateResourceConfigOutput, b bool) bool {
+		for _, result := range resp.Results {
+			fmt.Println(*result)
+		}
+
+		return true
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func queryResourceIDCommand() *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "query_by_id",
+		Short: "Query by resource id for resoucre",
+		Run:   queryResourceIDAction,
+	}
+
+	cmd.Flags().StringVarP(&aggregatorName, "aggregator-name", "", "AllAccounts", "AWS Config Aggretator Name")
+
+	return &cmd
+}
+
+func queryResourceIDAction(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		log.Fatalf("Usage: query_by_id <RESOURCE_ID>")
+	}
+
+	resourceID := args[0]
+
+	svc := configservice.New(config.Session())
+
+	queryTmpl := `
+SELECT
+  resourceId,
+  resourceName,
+  resourceType,
+  accountId,
+  arn,
+  configuration.instanceType,
+  tags,
+  availabilityZone,
+  configuration.state.name
+WHERE
+  resourceId = '%s'
+`
+
+	query := fmt.Sprintf(queryTmpl, resourceID)
 
 	input := &configservice.SelectAggregateResourceConfigInput{
 		ConfigurationAggregatorName: &aggregatorName,
